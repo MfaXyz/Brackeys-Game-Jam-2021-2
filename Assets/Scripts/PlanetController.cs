17879114c10a61533af2126bb53030c8ce8c4dde
MfaXyz <MfaXyzOfficial@gmail.com>
2021-08-24 16:02:39 +0430

    Add Hold Planets

---
+++ Assets/Scripts/PlanetController.cs
@@ -0,0 +1,49 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using PathCreation;
+
+public class PlanetController : MonoBehaviour
+{
+    [Header("Components")]
+    public PathCreator pathCreator;
+    [Header("Variables")]
+    public float speedMovement;
+    public float distanceTravelled;
+    public bool isReverse;
+
+    private void FixedUpdate()
+    {
+        distanceTravelled += speedMovement;
+
+        transform.position = pathCreator.path.GetPointAtDistance(distanceTravelled);
+        //transform.rotation = pathCreator.path.GetRotationAtDistance(distanceTravelled);
+    }
+
+    private void OnTriggerEnter2D(Collider2D col)
+    {
+        Debug.Log("Enter");
+        if (col.gameObject.CompareTag("LaserGun"))
+        {
+            speedMovement = 0;
+        }
+    }
+    public void OnTriggerExit2D(Collider2D other)
+    {
+        Debug.Log("Exit");
+        if (other.gameObject.CompareTag("LaserGun"))
+        {
+            if (isReverse)
+            {
+                speedMovement = 0.1f;
+                isReverse = false;
+            }
+            else
+            {
+                speedMovement = -0.1f; 
+                isReverse = true;
+            }
+        }
+    }
+}
--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -7,18 +7,36 @@ public class PlayerController : MonoBehaviour
 {
     [Header("Values")] 
     public Vector2 speedMovement;
+    public float maxParticleLifeTime;
 
     [Header("Components")] 
     public Rigidbody2D rb;
+    private ParticleSystem.MainModule _laserParticle;
+    public ParticleSystem laserParticle;
+    public CapsuleCollider2D laserCapsule;
 
     private void Awake()
     {
-        
+        _laserParticle = laserParticle.main;
     }
 
     private void FixedUpdate()
     {
         var movement = new Vector2(Input.GetAxis("Horizontal") * speedMovement.x, Input.GetAxis("Vertical") * speedMovement.y);
         rb.AddForce(movement, ForceMode2D.Impulse);
+    }
+
+    private void Update()
+    {
+        if (Input.GetMouseButton(0))
+        {
+            _laserParticle.startLifetime = maxParticleLifeTime;
+            laserCapsule.enabled = true;
+        }
+        else
+        {
+            _laserParticle.startLifetime = 0;
+            laserCapsule.enabled = false;
+        }
     }
 }
