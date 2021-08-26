af1c1ae879f0b01ef7b0028e95b1d119f743157c
MfaXyz <MfaXyzOfficial@gmail.com>
2021-08-26 09:27:56 +0430

    Add Main SpaceShip

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -8,8 +8,10 @@ public class PlayerController : MonoBehaviour
     [Header("Values")] 
     public float storageValue;
     public Vector2 speedMovement;
+    public float rotationSpeed;
     public float decreaseStorageValue;
-    public float maxParticleLifeTime;
+    public float maxGunParticleLifeTime;
+    public float maxEngineParticleLifeTime;
     public bool isMove;
 
     [Header("Components")] 
@@ -17,10 +19,19 @@ public class PlayerController : MonoBehaviour
     private ParticleSystem.MainModule _laserParticle;
     public ParticleSystem laserParticle;
     public CapsuleCollider2D laserCapsule;
+    private ParticleSystem.MainModule[] _enginesParticles = new ParticleSystem.MainModule[5];
+    public ParticleSystem[] enginesParticles;
 
     private void Awake()
     {
         _laserParticle = laserParticle.main;
+
+        for (var i = 0; i < 5; i++)
+        {
+            Debug.Log(i);
+            _enginesParticles[i] = enginesParticles[i].main;
+            Debug.Log(i);
+        }
     }
 
     private void FixedUpdate()
@@ -31,15 +42,22 @@ public class PlayerController : MonoBehaviour
             storageValue -= decreaseStorageValue;
         }
         
-        var movement = new Vector2(Input.GetAxis("Horizontal") * speedMovement.x, Input.GetAxis("Vertical") * speedMovement.y);
-        rb.AddForce(movement, ForceMode2D.Impulse);
+        var movement = new Vector2(0, Input.GetAxis("Vertical") * speedMovement.y);
+        rb.AddRelativeForce(movement, ForceMode2D.Impulse);
+        rb.AddTorque(Input.GetAxis("Horizontal") * -rotationSpeed,ForceMode2D.Impulse);
+
+        _enginesParticles[0].startLifetime = maxEngineParticleLifeTime * Input.GetAxis("Vertical");
+        _enginesParticles[1].startLifetime = maxEngineParticleLifeTime * Input.GetAxis("Vertical");
+        _enginesParticles[2].startLifetime = maxEngineParticleLifeTime * Input.GetAxis("Vertical");
+        _enginesParticles[3].startLifetime = maxEngineParticleLifeTime * -Input.GetAxis("Horizontal");
+        _enginesParticles[4].startLifetime = maxEngineParticleLifeTime * Input.GetAxis("Horizontal");
     }
 
     private void Update()
     {
         if (Input.GetMouseButton(0))
         {
-            _laserParticle.startLifetime = maxParticleLifeTime;
+            _laserParticle.startLifetime = maxGunParticleLifeTime;
             laserCapsule.enabled = true;
         }
         else
